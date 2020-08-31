def5f5c3385c38e9e2164b77594aa5b1ccd3229b
Kathrin Fausel <kathrin.fausel@linkit.tech>
2020-08-31 11:53:33 +0200

    add review after each step final

--- ProcessControl.cs
+++ ProcessControl.cs
@@ -38,24 +38,30 @@ namespace ProduktionssystemSimulation
             MttfPost = TimeSpan.FromMinutes(300 * (1 + smartService.Effektausmaß * smartService.MaschinenAusfallwkeit));
         }
 
-        public IEnumerable<SimSharp.Event> JobInProcess(Simulation env, Resource mPre, Resource mMain, Resource mPost, List<Job> jobs, Store pipe)
+        public IEnumerable<Event> JobInProcess(Simulation env, Resource mPre, Resource mMain, Resource mPost, List<Job> jobs)
         {
             foreach (Job job in jobs)
             {
                 env.Log("----------------------  START NEW JOB  ----------------------");
                 env.Log("JOB ID: {0}", job.ID);
-                yield return env.Process(Setup(env, mPre, mMain, mPost, job, pipe));
+                yield return env.Process(Setup(env, mPre, mMain, mPost, job));
             }
+            yield break;
         }
         int ProducedQuanity = 0;
-        int i = 0;
-        public IEnumerable<SimSharp.Event> Setup(Simulation env, Resource mPre, Resource mMain, Resource mPost, Job job, Store pipe)
+        Store producedProducts;
+        Store pipe;
+       
+        public IEnumerable<Event> Setup(Simulation env, Resource mPre, Resource mMain, Resource mPost, Job job)
         {
-            StoreGet get;
+            StoreGet getPipe;
+            
             foreach (Position position in job.Positions)
             {
+                producedProducts = new Store(env, position.Quantity);
+                pipe = new Store(env);
                 ProducedQuanity = 0;
-                i = 0;
+               
                 //SetUp
                 env.Log("---- SETUP PRODUCTYPE {0} ----", position.ID);
                 yield return env.Timeout(position.SetupPreMean);
@@ -64,28 +70,34 @@ namespace ProduktionssystemSimulation
                 {
                     pipe.Put(product);
                 }
-                Console.WriteLine(position.Products.Count);
                 
-                do
+                
+                    
+                   
+                while ( ProducedQuanity != position.Quantity)
                 {
-                    get = pipe.Get();
-                    yield return get;
-                    Product product = (Product)get.Value;
-                    if (i < position.Quantity)
+                    getPipe = pipe.Get();
+                // warte bis ein neues Teil produziert werden muss, oder alle Teile schon produziert sind
+                    yield return getPipe | producedProducts.WhenFull();
+                    if (producedProducts.Count == position.Quantity)
                     {
- 
-                        env.Process(Production(env, mPre, mMain, mPost, product, pipe));
-                    }else
-                    {
-                        yield return env.Process(Production(env, mPre, mMain, mPost, product, pipe));
+                       break;
                     }
-                    i++;
+                    
+                    Product product = (Product)getPipe.Value;
+                 
+                    env.Process(Production(env, mPre, mMain, mPost, product, pipe));
+
+
+
                 }
-                while (ProducedQuanity < position.Quantity);
+                    
+          
             }
+            yield break;
         }
 
-        public IEnumerable<SimSharp.Event> Production(Simulation env, Resource mPre, Resource mMain, Resource mPost, Product product, Store pipe)
+        public IEnumerable<Event> Production(Simulation env, Resource mPre, Resource mMain, Resource mPost, Product product, Store pipe)
         {
             var arrive = env.Now;
             env.Log("{0} ProductNo {1}: Here I am", arrive, product.ID);
@@ -100,7 +112,7 @@ namespace ProduktionssystemSimulation
                 product.Broken = false;
                 env.Log("Start new production of product {0}", product.ID);
                 pipe.Put(product);
-               
+                yield break;
             }
             else
             {
@@ -115,7 +127,7 @@ namespace ProduktionssystemSimulation
                     product.Broken = false;
                     env.Log("Start new production of product {0}", product.ID);
                     pipe.Put(product);
-                    
+                    yield break;
 
                 }
                 else
@@ -130,12 +142,13 @@ namespace ProduktionssystemSimulation
                         product.Broken = false;
                         env.Log("Start new production of product {0}", product.ID);
                         pipe.Put(product);
-                        
+                        yield break;
 
                     }
                     else
                     {
                         ProducedQuanity++;
+                        producedProducts.Put(product);
                         Console.WriteLine("Produced quantity: " + ProducedQuanity);
                     }
                     
@@ -143,47 +156,44 @@ namespace ProduktionssystemSimulation
             }
         }
 
-        public IEnumerable<SimSharp.Event> BreakMachinePreprocess(Simulation env)
+        public IEnumerable<Event> BreakMachinePreprocess(Simulation env)
         {
             while (true)
             {
                 // Ausfallwahrscheinlichkeit für M
                 var ausfall = env.RandExponential(MttfPost);
-                env.Log("Pre ===================== {0}", ausfall);
                 yield return env.Timeout(ausfall);
                 if (ProcessPre != null && !BrokenPre && ProcessPre.IsOk && ProcessPre.IsAlive)
                 {
-                    env.Log("Break Machine Pre");
+                    env.Log("================================================================Break Machine Pre");
                     ProcessPre.Interrupt();
                 }
             }
         }
 
-        public IEnumerable<SimSharp.Event> BreakMachineMainprocess(Simulation env)
+        public IEnumerable<Event> BreakMachineMainprocess(Simulation env)
         {
             while (true)
             {
                 var ausfall = env.RandExponential(MttfPost);
-                env.Log("Main ===================== {0}", ausfall);
                 yield return env.Timeout(ausfall);
                 if (ProcessMain != null && !BrokenMain && ProcessMain.IsOk && ProcessMain.IsAlive)
                 {
-                    env.Log("Break Machine Main");
+                    env.Log("================================================================Break Machine Main");
                     ProcessMain.Interrupt();   
                 }
             }
         }
-        public IEnumerable<SimSharp.Event> BreakMachinePostprocess(Simulation env)
+        public IEnumerable<Event> BreakMachinePostprocess(Simulation env)
         {
             while (true)
             {
                 // Ausfallwahrscheinlichkeit für M
                 var ausfall = env.RandExponential(MttfPost);
-                env.Log("Post ===================== {0}", ausfall);
                 yield return env.Timeout(ausfall);
                 if (ProcessPost != null && !BrokenPost && ProcessPost.IsOk && ProcessPost.IsAlive)
                 {
-                    env.Log("Break Machine Post");
+                    env.Log("================================================================Break Machine Post");
                     ProcessPost.Interrupt();
                 }
             }
@@ -192,7 +202,7 @@ namespace ProduktionssystemSimulation
         public void Simulate()
         { 
             env.Log("======= Production Factory ========");
-            var pipe = new Store(env);
+            
             Resource MPreprocess = new Resource(env, 1)
             {
                 Utilization = new TimeSeriesMonitor(env, name: "M1 Utilization"),
@@ -211,10 +221,10 @@ namespace ProduktionssystemSimulation
                 WaitingTime = new SampleMonitor(name: "M3 Waiting time", collect: true),
                 QueueLength = new TimeSeriesMonitor(env, name: "M3 Queue Length", collect: true),
             };
-            env.Process(JobInProcess(env, MPreprocess, MMainprocess, MPostprocess, Jobs, pipe));
-            //env.Process(BreakMachinePreprocess(env));
-            //env.Process(BreakMachineMainprocess(env));
-            //env.Process(BreakMachinePostprocess(env));
+            env.Process(JobInProcess(env, MPreprocess, MMainprocess, MPostprocess, Jobs));
+            env.Process(BreakMachinePreprocess(env));
+            env.Process(BreakMachineMainprocess(env));
+            env.Process(BreakMachinePostprocess(env));
 
             env.Run(TimeSpan.FromDays(2));
         }
