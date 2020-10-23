82fc8b0c3a10801006d691ee5f4379d54a770179
Kathrin Fausel <kathrin.fausel@linkit.tech>
2020-10-23 10:00:54 +0200

    Producttype umbenannt und Jobgenerator Klasse hinzugefügt

---
+++ Jobgenerator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimSharp;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ProduktionssystemSimulation
+{
+    public static class Jobgenerator
+    {  
+        private static Simulation env;
+
+        // Generiert die benötigenten Aufträge und speichert diese in einer Liste ab
+        public static (List<Job>, List<Job>) JobgeneratorS(Dictionary<string, double> inputData, int seed)
+        {
+            List<Job> Jobs1 = new List<Job>();
+            List<Job> Jobs2 = new List<Job>();
+            env = new Simulation(randomSeed: seed);
+
+            for (int i = 0; i < inputData["NumberOfJobs"]; i++)
+            {
+                List<Producttype> Positions1 = new List<Producttype>();
+                List<Producttype> Positions2 = new List<Producttype>();
+                for (int k = 1; k <= inputData["NumberPosition"]; k++)
+                {
+                    List<Product> Products1 = new List<Product>();
+                    List<Product> Products2 = new List<Product>();
+                    int ProductQuantity;
+
+                    do
+                    {
+                        ProductQuantity = (int)env.RandLogNormal2(inputData[$"QuantityMean{k}"], inputData[$"QuantitySigma{k}"]);
+                    } while (ProductQuantity <= 0);
+
+                    double MaterialCost = inputData[$"MaterialCostsPerProductMean{k}"];
+
+                    for (int j = 1; j <= ProductQuantity; j++)
+                    {
+                        Products1.Add(new Product(
+                            j,
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPreMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductMainMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPostMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPreSigma"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductMainSigma"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPostSigma"]),
+                            TimeSpan.FromDays(inputData["ReworkTimeMean"]),
+                            TimeSpan.FromDays(inputData["ReworkTimeSigma"])
+                            ));
+
+                        Products2.Add(new Product(
+                            j,
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPreMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductMainMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPostMean"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPreSigma"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductMainSigma"]),
+                            TimeSpan.FromDays(inputData["ProductionTimeProductPostSigma"]),
+                            TimeSpan.FromDays(inputData["ReworkTimeMean"]),
+                            TimeSpan.FromDays(inputData["ReworkTimeSigma"])
+                            ));
+                    }
+
+                    Positions1.Add(new Producttype(
+                        ProductQuantity,
+                        k,
+                        Products1,
+                        inputData[$"ScrapRatioPreMean{k}"],
+                        inputData[$"ScrapRatioMainMean{k}"],
+                        inputData[$"ScrapRatioPostMean{k}"],
+                        inputData[$"ReworkRatioPreMean{k}"],
+                        inputData[$"ReworkRatioMainMean{k}"],
+                        inputData[$"ReworkRatioPostMean{k}"],
+                        TimeSpan.FromDays(inputData[$"SetupTimeMean{k}"]),
+                        TimeSpan.FromDays(inputData[$"SetupTimeSigma{k}"]),
+                        MaterialCost,
+                        inputData[$"Price{k}"]
+                        ));
+
+                    Positions2.Add(new Producttype(
+                        ProductQuantity,
+                        k,
+                        Products1,
+                        inputData[$"ScrapRatioPreMean{k}"],
+                        inputData[$"ScrapRatioMainMean{k}"],
+                        inputData[$"ScrapRatioPostMean{k}"],
+                        inputData[$"ReworkRatioPreMean{k}"],
+                        inputData[$"ReworkRatioMainMean{k}"],
+                        inputData[$"ReworkRatioPostMean{k}"],
+                        TimeSpan.FromDays(inputData[$"SetupTimeMean{k}"]),
+                        TimeSpan.FromDays(inputData[$"SetupTimeSigma{k}"]),
+                        MaterialCost,
+                        inputData[$"Price{k}"]
+                        ));
+                }
+
+                Jobs1.Add(new Job(i, i + 1, Positions1));
+                Jobs2.Add(new Job(i, i + 1, Positions2));
+            }
+            return (Jobs1, Jobs2);
+        }
+    }
+}
---
+++ Producttype.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProduktionssystemSimulation
+{
+    public class Producttype
+    {
+        private List<Product> _products;
+        private TimeSpan _setupMean;
+        private TimeSpan _setupSigma;
+        private double _scrapPreMean;
+        private double _scrapMainMean;
+        private double _scrapPostMean;
+        private double _reworkPreMean;
+        private double _reworkMainMean;
+        private double _reworkPostMean;
+        private double _materialCost;
+        private double _price;
+        private int _quantity;
+        private int _totalProducedQuantity;
+        private int _id;
+
+        public Producttype(int quanity, int id, List<Product> products, double scrapPreMean, double scrapMainMean, double scrapPostMean, double reworkPreMean, double reworkMainMean, double reworkPostMean, TimeSpan setupMean, TimeSpan setupSigma, double materialCost, double price)
+        {
+            Quantity = quanity;
+            ID = id;
+            Products = products;
+            ScrapPreMean = scrapPreMean;
+            ScrapMainMean = scrapMainMean;
+            ScrapPostMean = scrapPostMean;
+            ReworkPreMean = reworkPreMean;
+            ReworkMainMean = reworkMainMean;
+            ReworkPostMean = reworkPostMean;
+            SetupMean = setupMean;
+            SetupSigma = setupSigma;
+            MaterialCost = materialCost;
+            Price = price;
+        }
+
+        public int Quantity { get => _quantity; set => _quantity = value; }
+        public List<Product> Products { get => _products; set => _products = value; }
+        public int ID { get => _id; set => _id = value; }
+        public double ScrapPreMean { get => _scrapPreMean; set => _scrapPreMean = value; }
+        public double ScrapMainMean { get => _scrapMainMean; set => _scrapMainMean = value; }
+        public double ScrapPostMean { get => _scrapPostMean; set => _scrapPostMean = value; }
+        public double ReworkPreMean { get => _reworkPreMean; set => _reworkPreMean = value; }
+        public double ReworkMainMean { get => _reworkMainMean; set => _reworkMainMean = value; }
+        public double ReworkPostMean { get => _reworkPostMean; set => _reworkPostMean = value; }
+        public TimeSpan SetupMean { get => _setupMean; set => _setupMean = value; }
+        public TimeSpan SetupSigma { get => _setupSigma; set => _setupSigma = value; }
+        public double MaterialCost { get => _materialCost; set => _materialCost = value; }
+        public double Price { get => _price; set => _price = value; }
+        public int TotalProducedQuantity { get => _totalProducedQuantity; set => _totalProducedQuantity = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Producttype producttype &&
+                   _quantity == producttype._quantity &&
+                   EqualityComparer<List<Product>>.Default.Equals(_products, producttype._products) &&
+                   _id == producttype._id;
+        }
+
+        public override string ToString()
+        {
+            return "Quantity: " + Quantity;
+        }
+
+    }
+}
